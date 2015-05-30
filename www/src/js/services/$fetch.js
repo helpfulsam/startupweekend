43dcf74ae2fca3c18843622fcef6480b6dcc15ea
Stephen Bunch <bunchclone@gmail.com>
2015-05-30 14:11:18 -0700

    fix $fetch

--- www/src/js/services/$fetch.js
+++ www/src/js/services/$fetch.js
@@ -1,51 +1,52 @@
 export default [
   '$window', '$timeout', 'moment', 'fetch',
 function( $window, $timeout, moment, fetch ) {
-  return function() {
-    /**
-     * @param {String} [url]
-     * @param {Object|Function} request
-     */
-    return function $fetch( url, request ) {
-      if ( typeof url === 'string' ) {
-        request.url = url;
-      } else {
-        request = url;
+  /**
+   * @param {String} [url]
+   * @param {Object|Function} request
+   */
+  return function $fetch( url, request ) {
+    if ( typeof url === 'string' ) {
+      if ( !request ) {
+        request = {};
       }
+      request.url = url;
+    } else {
+      request = url;
+    }
 
-      function build() {
-        return new Promise( function( resolve, reject ) {
-          if ( typeof request === 'function' ) {
-            try {
-              resolve( request() );
-            } catch ( err ) {
-              reject( err );
-            }
-          } else {
-            resolve( request );
-          }
-        });
-      }
-
+    function build() {
       return new Promise( function( resolve, reject ) {
-        var retries = 0;
-        var maxDelay = moment.duration( 30, 'seconds' ).asMilliseconds();
-
-        function send() {
-          build().then( function( request ) {
-            fetch.call( $window, request.url, request ).then( resolve ).catch( function( err ) {
-              // Assume lost internet connection?
-              retries += 1;
-              var delay = Math.pow( 2, retries ) * 100;
-              if ( delay > maxDelay ) {
-                delay = maxDelay
-              }
-              $timeout( send, delay );
-            });
-          }).catch( reject );
+        if ( typeof request === 'function' ) {
+          try {
+            resolve( request() );
+          } catch ( err ) {
+            reject( err );
+          }
+        } else {
+          resolve( request );
         }
-        send();
       });
-    };
+    }
+
+    return new Promise( function( resolve, reject ) {
+      var retries = 0;
+      var maxDelay = moment.duration( 30, 'seconds' ).asMilliseconds();
+
+      function send() {
+        build().then( function( request ) {
+          fetch.call( $window, request.url, request ).then( resolve ).catch( function( err ) {
+            // Assume lost internet connection?
+            retries += 1;
+            var delay = Math.pow( 2, retries ) * 100;
+            if ( delay > maxDelay ) {
+              delay = maxDelay
+            }
+            $timeout( send, delay );
+          });
+        }).catch( reject );
+      }
+      send();
+    });
   };
 }];
